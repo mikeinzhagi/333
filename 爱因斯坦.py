37e1d6f978f8bd124b8514d28d454c17f3bf3ac2
mikeinzhagi <mikenzhagi@163.com>
2020-08-26 21:25:20 +0800

    修改爱因斯坦.py

--- 爱因斯坦.py
+++ 爱因斯坦.py
@@ -1,4 +1,6 @@
 i = 0
+n = 0
+list = []
 while True:
     i += 1
     if i % 2 == 1:
@@ -6,5 +8,9 @@ while True:
             if i % 5 == 4:
                 if i % 6 == 5:
                     if i % 7 == 0:
-                        print(i)
-                        break
+                        print(i, end=' \n')
+                        n += 1
+                        list.append(i)
+                        if n >= 10:
+                            print(list)
+                            break
